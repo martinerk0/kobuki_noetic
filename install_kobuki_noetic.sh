943a332332133679fc3202e4d09206aba5f27da4
martinerk0 <1122-cervema@users.noreply.localhost>
2022-06-23 09:02:23 +0200

    added another missing dependencies

--- install_kobuki_noetic.sh
+++ install_kobuki_noetic.sh
@@ -1,8 +1,15 @@
 sudo apt install liborocos-kdl-dev
+sudo apt-get install ros-noetic-ecl-*
+sudo apt-get install libusb-dev
+sudo apt-get install libftdi-dev
+
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 git clone https://github.com/yujinrobot/kobuki.git
 git clone https://github.com/yujinrobot/yujin_ocs.git
+git clone https://github.com/yujinrobot/kobuki_msgs.git
+git clone https://github.com/yujinrobot/kobuki_core.git
+
 cd yujin_ocs
 mkdir save 
 mv yocs_cmd_vel_mux save
